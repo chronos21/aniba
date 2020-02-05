041cd29d7f12fa76306e0578055c6cee4ec3dc34
Cahya Isra <chsz77@googlemail.com>
2020-02-05 03:07:00 +0000

    fix routing

--- index.js
+++ index.js
@@ -245,9 +245,9 @@ app.get('/api/new-releases', async (req, res) => {
 	return res.status(200).json({ data });
 });
 
-// app.get('/*', (req, res) => {
-// 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 function saveFailure(obj, source) {
 	Failure.create({ text: JSON.stringify(obj), source })

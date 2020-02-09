7a09915c8d1c95387c391d4a7a997dd91dac44f2
Cahya Isra <chsz77@googlemail.com>
2020-02-09 20:01:39 +0700

    revamp mobile comment part ii

--- public/precache-manifest.fd1d860c6912834395e8bc6f113f0058.js
+++ public/precache-manifest.fd1d860c6912834395e8bc6f113f0058.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e350caecc1ddbbdc786f93be802f6909",
+    "revision": "b76cd1c7b974057a026c44c12641872c",
     "url": "/index.html"
   },
   {
-    "revision": "c0dc7ac9a3808b2c4149",
-    "url": "/static/css/main.d95f8ab0.chunk.css"
+    "revision": "6de8b38a585230532029",
+    "url": "/static/css/main.25d90543.chunk.css"
   },
   {
     "revision": "0de123bf270150fc7848",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.dad7e016.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c0dc7ac9a3808b2c4149",
-    "url": "/static/js/main.32362ba6.chunk.js"
+    "revision": "6de8b38a585230532029",
+    "url": "/static/js/main.70667fc6.chunk.js"
   },
   {
     "revision": "17233af3974891183882",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41caf2341d354aee0214985527800885.js"
+  "/precache-manifest.fd1d860c6912834395e8bc6f113f0058.js"
 );
 
 self.addEventListener('message', (event) => {

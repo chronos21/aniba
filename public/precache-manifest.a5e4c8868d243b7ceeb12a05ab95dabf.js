1fd8a845f41d3a926bbd8952a69ba041e02886b9
Cahya Isra <chsz77@googlemail.com>
2020-02-16 16:58:59 +0700

    fix memory leak on fe

---
+++ public/precache-manifest.a5e4c8868d243b7ceeb12a05ab95dabf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b779f68195e10217be81aba09d5992d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "145b84fbc49fa3284e73",
+    "url": "/static/css/main.67bfe6df.chunk.css"
+  },
+  {
+    "revision": "4686cfe2d29304f2dc94",
+    "url": "/static/js/2.e4a856eb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.e4a856eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "145b84fbc49fa3284e73",
+    "url": "/static/js/main.d3b1e016.chunk.js"
+  },
+  {
+    "revision": "17233af3974891183882",
+    "url": "/static/js/runtime-main.7f208323.js"
+  }
+]);

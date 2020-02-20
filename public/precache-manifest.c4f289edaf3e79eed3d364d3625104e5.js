0bf2b087cbeabae7ecdf4c9f82dfb7e7b95a4a1f
Cahya Isra <chsz77@googlemail.com>
2020-02-20 15:02:54 +0700

    fix auth redirect

--- public/precache-manifest.c4f289edaf3e79eed3d364d3625104e5.js
+++ public/precache-manifest.c4f289edaf3e79eed3d364d3625104e5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d3762b30be1f8056223589a62cc99b91",
+    "revision": "5f9e044f16c2e62f9744e7df0b594db9",
     "url": "/index.html"
   },
   {
-    "revision": "3a687c778c8b8dc0b7a3",
-    "url": "/static/css/main.c1d8afb5.chunk.css"
+    "revision": "f3dc0f462ac35a56b69c",
+    "url": "/static/css/main.a8561c37.chunk.css"
   },
   {
     "revision": "fb63d408346db15c298f",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b01e2f01.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3a687c778c8b8dc0b7a3",
-    "url": "/static/js/main.15ab7849.chunk.js"
+    "revision": "f3dc0f462ac35a56b69c",
+    "url": "/static/js/main.2f4e8f41.chunk.js"
   },
   {
     "revision": "17233af3974891183882",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e5d8caa5fd9d683f6efb7a3b1fa66b5.js"
+  "/precache-manifest.c4f289edaf3e79eed3d364d3625104e5.js"
 );
 
 self.addEventListener('message', (event) => {

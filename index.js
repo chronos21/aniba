428c5dfb5e918716110a5e52fe854571782707ab
Cahya Isra <cahya@wvi.co.id>
2020-12-13 00:07:27 +0700

    remove unused package

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const express = require('express');
-const moment = require('moment');
 const mainRoutes = require('./routes')
-
 const cors = require('cors')
-
 const app = express();
 const PORT = process.env.PORT || 8080;
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-app.locals.moment = moment;
-app.set('view engine', 'ejs');
 app.use(cors())
 app.use(express.static('public'));
-app.use(express.static('views'));
-
 app.use(express.json());
 
 app.use('/api', mainRoutes)

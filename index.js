0b95bfda34a303fd0a90b9db1c8138794c2067ea
Cahya Isra <cahya@wvi.co.id>
2021-03-07 17:32:01 +0700

    init ps3

--- index.js
+++ index.js
@@ -3,14 +3,39 @@ const mainRoutes = require('./routes')
 const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 8080;
+const crawler = require('./handlers/crawler')
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+app.set('view engine', 'ejs')
 
 app.use(cors())
 app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/api', mainRoutes)
+app.get('/', async (req, res) => {
+    let { q } = req.query;
+    let data = []
+    if(q){
+        data = await crawler.getSearch(q);
+    } else [
+        data = await crawler.getNewReleases()
+    ]
+    res.render('list', {data})
+})
+
+app.get('/:id', async (req, res) => {
+    let data = await crawler.getDetails(req.params.id);
+    res.render('episode', {data})
+})
+
+
+app.get('/series/:id', async (req, res) => {
+    let data = await crawler.getSeries(req.params.id);
+    res.render('series', {data})
+})
+
 app.get('*', (req, res) => {
     res.json({message: 'EMPTY. THERE IS NOTHING.'})
 })
